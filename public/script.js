b21ee3e7df4ca74ec799b04c399772ddb5163c33
vishwamparsa <vishwam@softnetworld.com>
2021-09-26 03:15:48 +0530

    Date: 26-sept,21

--- public/script.js
+++ public/script.js
@@ -544,7 +544,7 @@ function uploadFile(){
               cache: false,
               data: JSON.stringify(data1),
               success: function (data) {
-                //alert('Success!')
+                alert('Success!')
               }
               , error: function (jqXHR, textStatus, err) {
                 alert('text status ' + textStatus + ', err ' + err)
